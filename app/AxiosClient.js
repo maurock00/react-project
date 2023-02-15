33cf3888f7b7e8115ea03ca9ca067aed1ae0dbb5
maurock00 <mauricio@kushkipagos.com>
2023-02-15 15:52:01 -0500

    [002] Change endpoint

--- app/AxiosClient.js
+++ app/AxiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: process.env.BACKEND_URL || "",
+  baseURL:
+    process.env.BACKEND_URL || "https://react-project-backend.onrender.com/",
   timeout: 1000,
 });
 
